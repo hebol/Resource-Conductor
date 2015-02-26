c3f6d55872ad62e6121cfae7ee1912c203396707
oscar <oscars.mailbox@gmail.com>
2015-02-26 20:34:43 +0100

    First check-in. Newly created projects including socket.io 1.3.4

---
+++ Resource-Conductor-Services/Service-Registry/app.js
@@ -0,0 +1,53 @@
+var serverPort = 1234;
+var io         = require('socket.io')();
+
+var serviceList = {};
+
+var sizeObject = function(obj) {
+    var size = 0;
+    var key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            size++;
+        }
+    }
+    return size;
+};
+
+io.on('connection', function(socket) {
+    var registeredServices = {};
+    console.log('connecting:', socket.id);
+
+    function updateServiceList(target) {
+        console.log("Sending list of", sizeObject(serviceList), "services");
+        target.emit('updatedServiceList', serviceList);
+    }
+    updateServiceList(socket);
+
+    socket.on('registerService', function(service) {
+        console.log('Register received:', service);
+        serviceList[service.serviceId] = service;
+        registeredServices[service.serviceId] = service;
+        updateServiceList(io.sockets);
+    });
+
+    socket.on('unregisterService', function(serviceId) {
+        console.log('Unregister received:', serviceId);
+        delete serviceList[serviceId];
+        delete registeredServices[serviceId];
+        updateServiceList(io.sockets);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('disconnecting:', socket.id);
+        Object.keys(registeredServices).forEach(function (serviceId) {
+            console.log("Removing service", serviceId, "for disconnected client", socket.id);
+            delete serviceList[serviceId];
+            delete registeredServices[serviceId];
+        });
+        updateServiceList(io.sockets);
+    });
+});
+
+console.log("Has started server on port", serverPort);
+io.listen(serverPort);
