1701fd3ae52dad51a8b265450cf536973dd84ad7
martin <maartin@sjoblom.se>
2015-03-02 09:16:52 +0100

    Added ugly favicon

--- Resource-Conductor-Client/app.js
+++ Resource-Conductor-Client/app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use('/',        mapRoute);
 app.use('/report',  reportRoute);
