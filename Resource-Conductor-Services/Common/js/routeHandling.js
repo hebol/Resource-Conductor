50e5bb20e6b3f88fd786c48485c966578e46ad37
Martin Sjöblom <martin@sjoblom.se>
2015-03-21 12:01:24 +0100

    routing of ambulances started

---
+++ Resource-Conductor-Services/Common/js/routeHandling.js
@@ -0,0 +1,47 @@
+const routeDir = 'routes';
+var fs = require('fs');
+
+module.exports = {
+    dataFileExists: function (filename, callback) {
+        return fs.exists(filename, callback);
+    },
+
+    loadDataFile: function (filename, callback) {
+        console.log('loading route file', filename);
+        fs.readFile(filename, 'utf8', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            callback && callback(JSON.parse(data));
+        });
+    },
+
+    writeDataFile: function (filename, route) {
+        console.log('Hello', filename);
+        function writeFile() {
+            console.log('Will generate file', filename);
+            fs.writeFile(filename, JSON.stringify(route))
+        }
+
+        fs.exists(routeDir, function (exists) {
+            if (exists) {
+                writeFile();
+            } else {
+                fs.mkdir(routeDir, writeFile);
+            }
+        });
+    },
+    posToFilename: function (start, stop) {
+        function toFilename(pos1, pos2) {
+            return routeDir + '/' + pos1.latitude + '_' + pos1.longitude + '_' + pos2.latitude + '_' + pos2.longitude + ".json";
+        }
+
+        if (start.latitude > stop.latitude || (start.latitude === stop.latitude && start.longitude > stop.longitude)) {
+            return toFilename(start, stop);
+        } else {
+            return toFilename(stop, start);
+        }
+    }
+
+};
+
