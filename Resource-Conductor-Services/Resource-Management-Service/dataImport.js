f063b8639452e3773f751c33cf1bebff6f4b4db0
martin <maartin@sjoblom.se>
2015-03-30 22:13:28 +0200

    Added dependency

--- Resource-Conductor-Services/Resource-Management-Service/dataImport.js
+++ Resource-Conductor-Services/Resource-Management-Service/dataImport.js
@@ -11,7 +11,7 @@ module.exports = function() {
 
 var readCases = function() {
     parse({delimiter: ';'})
-}
+};
 
 var read = function(filename, callback) {
     fs.readFile(filename, 'iso-8859-1', function (err, data) {
