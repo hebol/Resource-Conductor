8e713b41ec7098b4872f18f589b7c75a28da70fc
Martin Sjöblom <martin@sjoblom.se>
2015-03-31 19:19:48 +0200

    Reading three types of data files

--- Resource-Conductor-Services/Resource-Management-Service/dataImport.js
+++ Resource-Conductor-Services/Resource-Management-Service/dataImport.js
@@ -1,5 +1,6 @@
 var parse = require('csv-parse');
 var fs = require('fs');
+var iconv = require('iconv-lite');
 
 module.exports = function() {
     var result = {
@@ -9,8 +10,36 @@ module.exports = function() {
     return result;
 };
 
+function readFile(parser, filename) {
+    var filename = __dirname + '/../data/' + filename;
+    console.log('Will open file in', filename);
+    var readStream = fs.createReadStream(filename);
+    var converterStream = iconv.decodeStream('latin1');
+    readStream.pipe(converterStream).pipe(parser);
+}
 var readCases = function() {
-    parse({delimiter: ';'})
+    var parser = parse({delimiter: ';', relax: true, columns: ['CallCenterId','CaseFolderId','CaseId',
+        'CreatedTime', 'FinishedTime','PreOrdered','PickupTime','CasePriority',
+        'CaseIndex1Name','CaseIndex2Name','latitude','longitude']}, function(err, data) {
+        err && console.log('err', err);
+        console.log('data', data);
+    });
+    readFile(parser, 'Hack_Case_2014-01-01.txt');
+};
+var readLog = function() {
+    var parser = parse({delimiter: ';', relax: true, columns: ['CallcenterId','CaseFolderId','CaseId','OrderNo','CreatedTime','LogText']}, function(err, data) {
+        err && console.log('err', err);
+        console.log('data', data);
+    });
+    readFile(parser, 'Hack_Log_2014-01-01.txt');
+};
+
+var readMissions= function() {
+    var parser = parse({delimiter: ';', relax: true, columns: ['CallcenterId','CaseFolderId','CaseId','MissionStarted','FinishedTime','ResourceCode','StationCode','FalseAlarm']}, function(err, data) {
+        err && console.log('err', err);
+        console.log('data', data);
+    });
+    readFile(parser, 'Hack_Mission_2014-01-01.txt');
 };
 
 var read = function(filename, callback) {
@@ -27,3 +56,6 @@ var read = function(filename, callback) {
     });
 };
 
+readMissions();
+readCases();
+readLog();
