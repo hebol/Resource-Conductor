8b46d3b2d0c4b29d27959277417582db697eb2fe
Martin Sjöblom <martin@sjoblom.se>
2015-05-20 08:52:11 +0200

    Create start.cmd
    Used for starting the app on windows

---
+++ start.cmd
@@ -0,0 +1,14 @@
+cd Resource-Conductor-Services/Service-Registry
+start nodemon app.js
+cd ../Time-Service
+start nodemon app.js
+cd ../Log-Service
+start nodemon app.js
+cd ../Route-Service
+start nodemon app.js
+cd ../Event-Simulator-Service
+start nodemon app.js
+cd ../Resource-Management-Service
+start nodemon app.js
+cd ../../Resource-Conductor-Client
+nodemon bin/www"
